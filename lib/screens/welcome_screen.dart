66a8c23ffcf200c9bebbe23b1fa74f10b32f5983
AlfiMostak <alfimostak99@gmail.com>
2021-11-26 14:28:52 +0600

    Step1

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,7 +1,5 @@
 import 'package:auction_app/screens/auctionGallery.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class WelcomeScreen extends StatefulWidget {
